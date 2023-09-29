720ce5d38f3a7d201e5ad17329d327372c488097
Saad Siddiqui <saadsiddiqui927@gmail.com>
2023-09-29 22:19:57 +0530

    integrate: blogs API into frontend

--- app/addNew/page.tsx
+++ app/addNew/page.tsx
@@ -1,16 +1,54 @@
+"use client";
+import { redirect } from "next/navigation";
+import { ChangeEvent, FormEvent, useState } from "react";
+
 const AddNew = () => {
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    if (!title || !description) {
+      return;
+    }
+    try {
+      const response = await fetch("http://localhost:3000/api/blogs", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          description,
+        }),
+      });
+      if (response.status === 201) {
+        setTitle("");
+        setDescription("");
+        redirect("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="mt-10">
       <h1>Add new blog</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           className="bg-gray-800 outline-0 border-none w-[100%] p-2 mb-2 rounded-md"
           placeholder="Enter title"
+          value={title}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value)
+          }
         />
         <textarea
           className="bg-gray-800 outline-0 border-none w-[100%] p-2 rounded-md"
           placeholder="Write something you want to share ..."
           rows={2}
+          value={description}
+          onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+            setDescription(e.target.value)
+          }
         />
         <button
           type="submit"
